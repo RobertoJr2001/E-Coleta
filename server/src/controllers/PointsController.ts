7adce766d3bb6953c5244d3a68fb7ca38e3f3838
Roberto Jr <junionascimento2001@gmail.com>
2020-06-09 03:28:58 -0300

    ...

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -2,6 +2,23 @@ import { Request,  Response} from 'express';
 import knex from '../database/connection';
 
 export default class Pointscontroller {
+  async show (request: Request, response: Response) {
+    const { id } = request.params;
+
+    const points = await knex('points').where('id', id).first();
+    
+    if (!points) {
+      return response.status(400).json({ message: 'Point not found' });
+    }
+
+    const items = await knex('items')
+      .join('point_items', 'items.id', '*', 'point_items.item_id')
+      .where('point_items.point_id', id)
+      .select('items.title');
+
+    return response.json({ points, items});
+  }
+
   async create (request: Request, response: Response) {
     const {
       name,
